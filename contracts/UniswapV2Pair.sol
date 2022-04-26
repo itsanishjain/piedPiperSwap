844eedb4f4fcd517f81ef393771391b465d24541
anish jain <helloanishjain@gmail.com>
2022-04-26 23:44:34 +0530

    intiated

---
+++ contracts/UniswapV2Pair.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "../interfaces/IUniswapV2ERC20.sol";
+import "../interfaces/IUniswapV2Pair.sol";
+
+
+
+// contract UniswapV2Pair is IUniswapV2ERC20,IUniswapV2Pair {
+
+   
+// }
