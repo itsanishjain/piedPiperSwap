2fd2dd37cb95096690a774d82f30be53281d2131
anish jain <helloanishjain@gmail.com>
2022-04-30 09:01:46 +0530

    added swap

--- contracts/UniswapV2Pair.sol
+++ contracts/UniswapV2Pair.sol
@@ -277,6 +277,89 @@ contract UniswapV2Pair is IUniswapV2Pair, IUniswapV2ERC20 {
         }
     }
 
-
     // DONE 155 Lines of code
+
+    function swap(
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address to,
+        bytes calldata data
+    ) external lock {
+        require(
+            amount0Out > 0 || amount1Out,
+            "Uniswap: INSUFICIENT_OUTPUT_AMOUNT"
+        );
+        (uint112 _reserve0, uint112 _reserve1, ) = getReserves();
+        require(
+            amount0Out < _reserve0 && amount1Out < _reserve1,
+            "Uniswap: INSUFICIENT_LIQUIDITY"
+        );
+
+        uint256 balance0;
+        uint256 balance1;
+
+        {
+            // scope for _token{0,1}, avoids stack too deep errors
+
+            address _token0 = token0;
+            address _token1 = token1;
+
+            require(to != _token0 && to != _token1, "Uniswap: INVALID_TO");
+
+            if (amount0Out > 0) {
+                _safeTransfer(_token0, to, amount0Out);
+            }
+            if (amount1Out > 0) {
+                _safeTransfer(_token1, to, amount1Out);
+            }
+            if (data.length > 0) {
+                IUniswapV2Callee(to).uniswapV2Call(
+                    msg.sender,
+                    amount0Out,
+                    amount1Out,
+                    data
+                );
+            }
+
+            balance0 = IERC20(_token0).balanceOf(address(this));
+            balance1 = IERC20(_token1).balanceOf(address(this));
+        }
+
+        uint256 amount0In = balance0 > _reserve0 - amount0Out
+            ? balance0 - (_reserve0 - amount0Out)
+            : 0;
+
+        uint256 amount1In = balance1 > _reserve1 - amount1Out
+            ? balance1 - (_reserve1 - amount1Out)
+            : 0;
+
+        require(
+            amount0In > 0 || amount1In > 0,
+            "UniswapV2: INSUFFICIENT_INPUT_AMOUNT"
+        );
+
+        {
+            uint256 balance0Adjusted = balance0 * 1000 - amount0In * 3;
+
+            uint256 balance1Adjusted = balance1 * 1000 - amount1In * 3;
+
+            require(
+                balance0Adjusted * balance1Adjusted >=
+                    uint256(_reserve1) * uint256(_reserve1) * 1000**2,
+                "UniswapV2: K"
+            );
+        }
+
+        _update(balance0, balance1, _reserve0, _reserve1);
+
+        emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
+    }
+
+    // DONE 185 lines of code 
+
+    /*
+        still things like adjusted balance is not clear
+     */
 }
+
+
